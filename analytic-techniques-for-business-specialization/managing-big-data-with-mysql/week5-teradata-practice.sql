f05b97bcfae96b8f5d16d604e3c47d101e3c533e
Ming Chen <ming.chen0919@gmail.com>
2018-07-04 18:26:15 -0400

    update

--- analytic-techniques-for-business-specialization/managing-big-data-with-mysql/week5-teradata-practice.sql
+++ analytic-techniques-for-business-specialization/managing-big-data-with-mysql/week5-teradata-practice.sql
@@ -11,37 +11,22 @@ GROUP BY year_number, month_number;
 /* 2. Use a CASE statement within an aggregate function to determine which sku
 had the greatest total sales during the combined summer months of June, July, and August. */
 
-/* get year, month, sale price and a fourth column to indicate if this is a summer month,
-and filter output by summer_months*/
+/* step 0: the final table should have these columns: sku, year, total summer sale price.
+   step 1: mark a transaction if it occurred in summer or not (CASE).
+   step 2: filter only summer transactions (WHERE).
+   step 3: aggregate sale price by sku, year (GROUP BY)
+   step 4: sort by aggregated sale price (ORDER BY)
+ */
 SELECT
     sku,
     EXTRACT(YEAR FROM saledate) AS year_num,
-    EXTRACT(MONTH FROM saledate) AS month_num,
-    sprice,
+    SUM(sprice) AS total_sprice,
     CASE
       WHEN EXTRACT(MONTH FROM saledate) IN (6,7,8) THEN 'summer'
       ELSE 'not summer'
       END AS summer_months
 FROM trnsact
-WHERE summer_months='summer';
+WHERE summer_months='summer'
+GROUP BY sku, year_num, summer_months
+ORDER BY total_sprice DESC;
 
-/* group by sku, year, month from previous output and aggregate sprice*/
-SELECT
-  summer_sales.sku,
-  summer_sales.year_num,
-  summer_sales.month_num,
-  SUM(summer_sales.sprice) AS total_sprice
-FROM
-  (SELECT
-    sku,
-    EXTRACT(YEAR FROM saledate) AS year_num,
-    EXTRACT(MONTH FROM saledate) AS month_num,
-    sprice,
-    CASE
-      WHEN EXTRACT(MONTH FROM saledate) IN (6,7,8) THEN 'summer'
-      ELSE 'not summer'
-      END AS summer_months
-  FROM trnsact
-  WHERE summer_months='summer') AS summer_sales
-GROUP BY 1, 2, 3
-ORDER BY total_sprice;
